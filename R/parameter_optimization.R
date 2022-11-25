d4bb2ff49a6ff48f5af079e8fd6ee3407d8b041a
Peevin <93245035+Peevin@users.noreply.github.com>
2022-11-25 20:30:21 +0800

    Update parameter_optimization.R

--- R/parameter_optimization.R
+++ R/parameter_optimization.R
@@ -28,6 +28,10 @@
 model_evaluation_optimization = function(x, source_names, algorithm, correct_topology, lr_network, sig_network, gr_network, settings, secondary_targets = FALSE, remove_direct_links = "no",damping_factor = NULL,...){
 
   requireNamespace("dplyr")
+  
+  print('[x]:')
+  print(x)
+  
   if (!is.null(damping_factor) & is.null(x$damping_factor)){ # for the case damping factor is a fixed parameter
     x$damping_factor = damping_factor
   }
